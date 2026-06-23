906e1c7c1fffb044a910de795e30d2fed9746dd7
Ahmed-Yehia-Sokar <ahmedyehiasokar94@gmail.com>
2026-06-23 11:21:25 +0300

    Initial Commit

---
+++ RickAndMortyWiki/RickAndMortyWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWikiApp.swift
+//  RickAndMortyWiki
+//
+//  Created by Admin on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
